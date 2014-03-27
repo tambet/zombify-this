dc70427b21ab2002add7f6c7f903495f50550c97
Tambet Masik <tambet.masik@gmail.com>
2014-03-27 09:59:22 +0200

    Click to shoot

--- zombify.js
+++ zombify.js
@@ -1,6 +1,11 @@
 function addZombie() {
   var div = document.createElement('div');
   div.setAttribute('id', 'zombie');
+
+  div.addEventListener('click', function (e) {
+    div.parentNode.removeChild(div);
+  });
+
   document.body.appendChild(div);
 }
 addZombie();
